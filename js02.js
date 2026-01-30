7eb983e9455615d105ba8edbccd76bc91b8b665b
Remiboiz <iredomo09@gmail.com>
2026-01-30 19:07:13 +0000

    Firsthing

---
+++ js02.js
@@ -0,0 +1,23 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Iremide Omoniyi
+      Date:   1/30/26
+
+      Filename: js02.js
+ */
+
+// set the form's default values
+// setup the form when the page loads 
+      window.addEventListener("load", setupForm);
+function setupForm()  {
+      document.getElementById("photoNum").value = 1;
+      document.getElementById("photoHrs").value = 2;
+      document.getElementById("makeBook").checked = false;
+      document.getElementById("photoRights").checked = false;
+      document.getElementById("pgotoDist").value = 0;
+}
+
